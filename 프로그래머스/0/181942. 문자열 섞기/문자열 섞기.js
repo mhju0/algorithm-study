e92954b99cb98018008eee4cd5ed12e5c9ff447c
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-03-13 14:49:06 +0900

    [level 0] Title: 문자열 섞기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -0,0 +1,12 @@
+function solution(str1, str2) {
+    var answer = '';
+    // str1.(홀) str2.(짝)?
+    // 조건: 길이 똑같음
+    stringLength = str1.length;
+    for (i=0; i < stringLength; i++){
+        answer += str1[i];
+    answer += str2[i];
+    }
+        
+    return answer;
+}

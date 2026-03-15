4db482fd36c66d78bf8999cd0e9ddf5f10d48ee5
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-03-15 16:11:05 +0900

    [level 0] Title: 코드 처리하기, Time: 33.02 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -1,26 +1,17 @@
 function solution(code) {
-  // code 주어짐
-  let answer = "";
-  let mode = 0;
-  // code 차례로 읽음. 1이 나오면 mode 변화  (for loop) 사용하고 안에 if? or ? :
-  for (i = 0; i < code.length; i++) {
-    if (mode === 0) {
-      if (code[i] === '1') {
-        mode = 1;
-      } else {
-        if (i % 2 === 0) {
-          answer = answer + code[i];
+    let answer = '';
+    let mode = 0;
+    for (let i = 0; i < code.length; i++){
+        if(code[i] ==='1') {
+            // switch mode between 1 and 0;
+            mode = 1 - mode;
+            // check i is even or odd using mode; 
+            // mode 0 returns i is even, mode 1 returns i is odd
+        } else if (i % 2 === mode) {
+            answer += code[i];
         }
-      }
-    } else {
-      if (code[i] === '1') {
-        mode = 0;
-      } else {
-        if (i % 2 !== 0) {
-          answer = answer + code[i];
-        }
-      }
     }
-  }
-  return answer === '' ? 'EMPTY' : answer;
+    // Use OR logical operator because '' is false
+    // Shorter than return answer === '' ? 'EMPTY' : answer
+    return answer || 'EMPTY';
 }

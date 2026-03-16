abde74246b6ec4bc0c6c0e0b72cdd33a9a4216e0
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-03-16 19:46:29 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,6 +1,7 @@
 function solution(num_list) {
-  num_list.at(-1) > num_list.at(-2)
-    ? num_list.push(num_list.at(-1) - num_list.at(-2))
-    : num_list.push(num_list.at(-1) * 2);
-  return num_list;
+    const last = num_list.at(-1);
+    const prev = num_list.at(-2);
+    const newValue = last > prev ? last - prev : last * 2;
+    num_list.push(newValue);
+    return num_list
 }

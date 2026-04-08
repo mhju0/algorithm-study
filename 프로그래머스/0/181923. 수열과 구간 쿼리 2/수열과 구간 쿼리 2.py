48e6242420f9e280ae0abd87e743bc6d788171ed
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-08 17:45:20 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 9.92 ms, Memory: 9.46 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -0,0 +1,6 @@
+def solution(arr, queries):
+    answer = []
+    for s, e, k in queries:
+        filtered = [x for x in arr[s:e+1] if x > k]
+        answer.append(min(filtered) if filtered else -1)
+    return answer

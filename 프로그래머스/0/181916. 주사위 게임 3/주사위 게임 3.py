c3baaf12e78e1f1991dedf980f9631769ef0f15a
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-09 18:26:18 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,24 +1,21 @@
 def solution(a, b, c, d):
     counts = {}
-    for n in [a,b,c,d]:
+    for n in [a, b, c, d]:
         counts[n] = counts.get(n, 0) + 1
-        
-    diceVals = list(counts.values())
-    diceKeys = list(counts.keys())
-    
-    size = len(set([a,b,c,d]))
-    
+
+    size = len(set([a, b, c, d]))
+
     if size == 1:
         return 1111 * a
     if size == 4:
-        return min(a,b,c,d)
-    if size == 2 and 3 in diceVals:
-        p = next(k for k,v in counts.items() if v == 3)
-        q = next(k for k,v in counts.items() if v == 1)
+        return min(a, b, c, d)
+    if size == 2 and 3 in counts.values():
+        p = next(k for k, v in counts.items() if v == 3)
+        q = next(k for k, v in counts.items() if v == 1)
         return (10 * p + q) ** 2
     if size == 2:
         p, q = [k for k, v in counts.items() if v == 2]
-        return (p+q) * abs(p-q)
+        return (p + q) * abs(p - q)
     if size == 3:
-        q, r = [k for k,v in counts.items() if v == 1]
+        q, r = [k for k, v in counts.items() if v == 1]
         return q * r

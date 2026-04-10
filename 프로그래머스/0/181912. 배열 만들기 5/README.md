704a33b4e9ff486c098f195729cdfbd6fd24ad26
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-10 12:49:58 +0900

    [level 0] Title: 배열 만들기 5, Time: 3.33 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/README.md
+++ 프로그래머스/0/181912. 배열 만들기 5/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 5 - 181912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181912?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181912) 
 
 ### 성능 요약
 
-메모리: 11.1 MB, 시간: 3.55 ms
+메모리: 11.1 MB, 시간: 3.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 10일 12:47:33
+2026년 04월 10일 12:49:54
 
 ### 문제 설명
 
--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -1,6 +1,9 @@
+# def solution(intStrs, k, s, l):
+#     answer = []
+#     for n in intStrs:
+#         if int(n[s:s+l]) > k:
+#             answer.append(int(n[s:s+l]))
+#     return answer
+
 def solution(intStrs, k, s, l):
-    answer = []
-    for n in intStrs:
-        if int(n[s:s+l]) > k:
-            answer.append(int(n[s:s+l]))
-    return answer
+    return [int(n[s:s+l]) for n in intStrs if int(n[s:s+l]) > k]

aa662eeb1ef65d12ba9383009f4c83abee7183ed
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-10 17:21:37 +0900

    [level 0] Title: 2의 영역, Time: 7.81 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,6 +1,6 @@
 def solution(arr):
     list_answer = [i for i, v in enumerate(arr) if v == 2]
-    if not list_answer:
+    if 2 not in arr:
         return [-1]
     else:
         return arr[list_answer[0]:list_answer[-1]+1]

4d476cfa021289ed5f9ab5cabe6d069bf61a8645
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-10 16:14:45 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
@@ -1,6 +1,2 @@
 def solution(start_num, end_num):
-    # list comprehension 
-    # return [i for i in range(start_num, end_num + 1)
-    # ----------------------------------------------------
-    # list() range() method
-    return list(range(start_num, end_num+1))
+    return list(range(start_num, end_num - 1, -1))

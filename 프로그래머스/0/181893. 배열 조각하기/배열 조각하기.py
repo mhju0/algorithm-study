8af59b1d07007d0ed0e9666cc7ddcec52da4a52c
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-11 17:52:54 +0900

    [level 0] Title: 배열 조각하기, Time: 2.13 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -1,8 +1,13 @@
-def solution(arr, query):
-    for i,v in enumerate(query):
-        if i % 2 == 0:
-            arr = arr[:v+1]
-        else:
-            arr = arr[v:]
-    return arr
+# def solution(arr, query):
+#     # enumerate 연습
+#     for i,v in enumerate(query):
+#         if i % 2 == 0:
+#             arr = arr[:v+1]
+#         else:
+#             arr = arr[v:]
+#     return arr
             
+
+from functools import reduce
+def solution(arr, query):
+    return reduce(lambda a, t: a[:t[1]+1] if t[0]%2==0 else a[t[1]:], enumerate(query), arr)

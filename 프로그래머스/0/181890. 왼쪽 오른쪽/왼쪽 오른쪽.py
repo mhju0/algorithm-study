f426e029ea9e30e2784154c8799b4630af4c8816
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-17 10:47:48 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,10 +1,7 @@
 def solution(str_list):
-    loc_l = str_list.index('l') if 'l' in str_list else float('inf')  
-    loc_r = str_list.index('r') if 'r' in str_list else float('inf')
-
-    if loc_l == float('inf') and loc_r == float('inf'):
-        return []
-    elif loc_l <= loc_r:
-        return str_list[:loc_l]
-    else:
-        return str_list[loc_r+1:]
+    for i, s in enumerate(str_list):
+        if s == 'l':
+            return str_list[:i]
+        elif s == 'r':
+            return str_list[i+1:]
+    return []

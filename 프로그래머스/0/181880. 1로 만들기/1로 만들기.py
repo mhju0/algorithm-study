fe1c09ef7656884da0f8d47d9e0c5ddbff0dfb8f
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-17 17:36:51 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -1,10 +1,7 @@
 def solution(num_list):
     counter = 0
-    for i,v in enumerate(num_list):
+    for v in num_list:
         while v != 1:
-            if v % 2 == 0:
-                v //= 2
-            elif v % 2 == 1:
-                v = (v-1) // 2
+            v = v // 2 if v % 2 == 0 else (v-1) // 2
             counter += 1
     return counter

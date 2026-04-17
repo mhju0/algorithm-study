16cef444d5c81243bdf54644a5ce870e42a5693a
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-17 11:11:08 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,9 @@
+# def solution(todo_list, finished):
+#     unfinished = []
+#     for n, m in zip(todo_list, finished):
+#         if m == False:
+#             unfinished.append(n)
+#     return unfinished
+
 def solution(todo_list, finished):
-    unfinished = []
-    for n, m in zip(todo_list, finished):
-        if m == False:
-            unfinished.append(n)
-    return unfinished
+    return [n for n,m in zip(todo_list, finished) if not m]

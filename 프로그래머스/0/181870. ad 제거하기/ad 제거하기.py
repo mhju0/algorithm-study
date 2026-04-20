c01a8b833b4ee398dd3da64e631d0bd27c502dba
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-20 15:56:03 +0900

    [level 0] Title: ad 제거하기, Time: 0.09 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -1,6 +1,2 @@
 def solution(strArr):
-    answer = []
-    for i,v in enumerate(strArr):
-        if "ad" not in v:
-            answer.append(v)
-    return answer
+    return [v for v in strArr if "ad" not in v]

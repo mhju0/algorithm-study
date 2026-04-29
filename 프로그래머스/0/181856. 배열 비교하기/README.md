86c88b6b4b8b425d90b77fe0102ad7a94f8da605
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-29 10:50:15 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/README.md
+++ 프로그래머스/0/181856. 배열 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 비교하기 - 181856 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.17 MB, 시간: 0.01 ms
+메모리: 9.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 29일 10:45:52
+2026년 04월 29일 10:50:13
 
 ### 문제 설명
 
--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -1,16 +1,10 @@
 def solution(arr1, arr2):
-    len1 = len(arr1)
-    len2 = len(arr2)
-    sum1 = sum(arr1)
-    sum2 = sum(arr2)
-    if len1 > len2:
-        return 1
-    elif len1 < len2:
-        return -1
+    len1, len2 = len(arr1), len(arr2)
+    sum1, sum2 = sum(arr1), sum(arr2)
+    if len1 != len2:
+        return 1 if len1 > len2 else -1
     else:
-        if sum1 > sum2:
-            return 1
-        elif sum1 < sum2:
-            return -1
-        else:
+        if sum1 == sum2:
             return 0
+        else:
+            return 1 if sum1 > sum2 else -1

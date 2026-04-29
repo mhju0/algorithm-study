be9a499019d5712bb54165510e94130d795236a4
mhju0 <166675154+mhju0@users.noreply.github.com>
2026-04-29 17:55:58 +0900

    [level 0] Title: 문자열 묶기, Time: 13.33 ms, Memory: 15.3 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/README.md
+++ 프로그래머스/0/181855. 문자열 묶기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 묶기 - 181855 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181855) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181855?language=python3) 
 
 ### 성능 요약
 
-메모리: 16.8 MB, 시간: 17.39 ms
+메모리: 15.3 MB, 시간: 13.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 29일 17:54:14
+2026년 04월 29일 17:55:56
 
 ### 문제 설명
 
--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,12 +1,3 @@
+from collections import Counter
 def solution(strArr):
-    str_length = []
-    for string in strArr:
-        str_length.append(len(string))
-    
-    count_length = {}
-    for i in str_length:
-        if i in count_length:
-            count_length[i] += 1
-        else:
-            count_length[i] = 1
-    return max(count_length.values())
+    return max(Counter(len(s) for s in strArr).values())
